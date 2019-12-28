c18cdb29bf4a7c8393f722bd45233b11e22609ba
Katarine1 <kba.2879@gmail.com>
2019-12-28 19:17:14 -0300

    Lamp Project with C# in Console

---
+++ App1_Lamp/Model/Lamp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Lamp.Model
+{
+    public class Lamp
+    {
+        private string Status { get; set; }
+        private string Adjustment { get; set; }
+
+        public Lamp() { }
+
+        public Lamp(string state)
+        {
+            Status = state;
+        }
+
+        public String AdjustmentLamp()
+        {
+            if(Status == "Off")
+            {
+                Adjustment = "0%";
+                return "Adjustment : " + Adjustment;
+            }
+            else if (Status == "On")
+            {
+                Adjustment = "100%";
+                return "Adjustment : " + Adjustment;
+            }
+            else if(Status == "Half-light")
+            {
+                Adjustment = "50%";
+                return "Adjustment : " + Adjustment;
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "State : " + Status;
+        }
+    }
+}
---
+++ App1_Lamp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using App1_Lamp.Model;
+
+namespace App1_Lamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.Write("\t\tQuantity : ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\n\t\tNumber " + (i + 1) + " :\n");
+                Console.Write("\t\tEnter Lamp Status (On, Off, or Half-light): ");
+                string status = Console.ReadLine();
+
+                Lamp lamp = new Lamp(status);
+                string adjustment = lamp.AdjustmentLamp();
+
+                Console.WriteLine("\t\tLamp : " + status + "\n\t\t" + adjustment);
+            }                
+        }
+    }
+}
